65b1a9da615b7120dd1452fde270af010ab184e7
Chunar <m18742021187@163.com>
2020-07-17 21:05:24 +0800

    add mysql operation

--- ChartDemo/chart/models.py
+++ ChartDemo/chart/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Voltage(models.Model):
+	added_date = models.DateTimeField()
+	value = models.DecimalField(max_digits=6, decimal_places=2)
+
+	# def __str__(self):
+	# 	return self.value
+
+	class Meta():
+		verbose_name_plural = 'Voltage'
--- ChartDemo/chart/views.py
+++ ChartDemo/chart/views.py
@@ -9,145 +9,153 @@ from pyecharts import options as opts
 
 from django.shortcuts import render
 
+from . import models
+
 # Create your views here.
 def response_as_json(data):
-    json_str = json.dumps(data)
-    response = HttpResponse(
-        json_str,
-        content_type="application/json",
-    )
-    response["Access-Control-Allow-Origin"] = "*"
-    return response
+	json_str = json.dumps(data)
+	response = HttpResponse(
+		json_str,
+		content_type="application/json",
+	)
+	response["Access-Control-Allow-Origin"] = "*"
+	return response
 
 
 def json_response(data, code=200):
-    data = {
-        "code": code,
-        "msg": "success",
-        "data": data,
-    }
-    return response_as_json(data)
+	data = {
+		"code": code,
+		"msg": "success",
+		"data": data,
+	}
+	return response_as_json(data)
 
 
 def json_error(error_string="error", code=500, **kwargs):
-    data = {
-        "code": code,
-        "msg": error_string,
-        "data": {}
-    }
-    data.update(kwargs)
-    return response_as_json(data)
+	data = {
+		"code": code,
+		"msg": error_string,
+		"data": {}
+	}
+	data.update(kwargs)
+	return response_as_json(data)
 
 
 JsonResponse = json_response
 JsonError = json_error
 
 
+def get_data():
+	value = models.Voltage.objects.filter(added_date='2020-07-17 20:46:33.000000')
+	print(value[0].value)
+	print(type(value[0].value))
+
 def current() -> Line:
-    line = (
-        Line()
-        .add_xaxis(list(range(10)))
-        .add_yaxis(series_name="A相电流", 
+	line = (
+		Line()
+		.add_xaxis(list(range(10)))
+		.add_yaxis(series_name="A相电流", 
 				   y_axis=[randrange(0, 100) for _ in range(10)],
 				   is_smooth=True,)
-        .add_yaxis(series_name="B相电流", 
+		.add_yaxis(series_name="B相电流", 
 				   y_axis=[randrange(0, 100) for _ in range(10)],
 				   is_smooth=True,)
-        .add_yaxis(series_name="C相电流", 
+		.add_yaxis(series_name="C相电流", 
 				   y_axis=[randrange(0, 100) for _ in range(10)],
 				   is_smooth=True,)
-        .set_global_opts(
-            title_opts=opts.TitleOpts(title="电流"),
-            xaxis_opts=opts.AxisOpts(type_="value"),
-            yaxis_opts=opts.AxisOpts(type_="value")
-        )
-        .dump_options_with_quotes()
-    )
-    return line
+		.set_global_opts(
+			title_opts=opts.TitleOpts(title="电流"),
+			xaxis_opts=opts.AxisOpts(type_="value"),
+			yaxis_opts=opts.AxisOpts(type_="value")
+		)
+		.dump_options_with_quotes()
+	)
+	return line
 
 def voltage() -> Line:
-    line = (
-        Line()
-        .add_xaxis(list(range(10)))
-        .add_yaxis(series_name="", 
+	get_data()
+	line = (
+		Line()
+		.add_xaxis(list(range(10)))
+		.add_yaxis(series_name="", 
 				   y_axis=[randrange(0, 100) for _ in range(10)],
 				   is_smooth=True,)
-        .set_global_opts(
-            title_opts=opts.TitleOpts(title="电压"),
-            xaxis_opts=opts.AxisOpts(type_="value"),
-            yaxis_opts=opts.AxisOpts(type_="value")
-        )
-        .dump_options_with_quotes()
-    )
-    return line
+		.set_global_opts(
+			title_opts=opts.TitleOpts(title="电压"),
+			xaxis_opts=opts.AxisOpts(type_="value"),
+			yaxis_opts=opts.AxisOpts(type_="value")
+		)
+		.dump_options_with_quotes()
+	)
+	return line
 
 def temperature(title) -> Line:
-    line = (
-        Line()
-        .add_xaxis(list(range(10)))
-        .add_yaxis(series_name="", 
+	line = (
+		Line()
+		.add_xaxis(list(range(10)))
+		.add_yaxis(series_name="", 
 				   y_axis=[randrange(0, 100) for _ in range(10)],
 				   is_smooth=True,)
-        .set_global_opts(
-            title_opts=opts.TitleOpts(title=title),
-            xaxis_opts=opts.AxisOpts(type_="value"),
-            yaxis_opts=opts.AxisOpts(type_="value")
-        )
-        .dump_options_with_quotes()
-    )
-    return line
+		.set_global_opts(
+			title_opts=opts.TitleOpts(title=title),
+			xaxis_opts=opts.AxisOpts(type_="value"),
+			yaxis_opts=opts.AxisOpts(type_="value")
+		)
+		.dump_options_with_quotes()
+	)
+	return line
 
 
 class CurrentChart(APIView):
-    def get(self, request, *args, **kwargs):
-        return JsonResponse(json.loads(current()))
-    
+	def get(self, request, *args, **kwargs):
+		return JsonResponse(json.loads(current()))
+	
 class VoltageChart(APIView):
-    def get(self, request, *args, **kwargs):
-        return JsonResponse(json.loads(voltage()))
+	def get(self, request, *args, **kwargs):
+		return JsonResponse(json.loads(voltage()))
 
 class TemperatureFirstChart(APIView):
-    def get(self, request, *args, **kwargs):
-        return JsonResponse(json.loads(temperature("前端绕组")))
+	def get(self, request, *args, **kwargs):
+		return JsonResponse(json.loads(temperature("前端绕组")))
 
 class TemperatureSecondChart(APIView):
-    def get(self, request, *args, **kwargs):
-        return JsonResponse(json.loads(temperature("后端绕组")))
+	def get(self, request, *args, **kwargs):
+		return JsonResponse(json.loads(temperature("后端绕组")))
 
 class TemperatureThirdChart(APIView):
-    def get(self, request, *args, **kwargs):
-        return JsonResponse(json.loads(temperature("冷却水")))
+	def get(self, request, *args, **kwargs):
+		return JsonResponse(json.loads(temperature("冷却水")))
 
 cnt = 9
 
 
 class ChartUpdateView(APIView):
-    def get(self, request, *args, **kwargs):
-        global cnt
-        cnt = cnt + 1
-        return JsonResponse({"name": cnt, "value": randrange(0, 100)})
+	def get(self, request, *args, **kwargs):
+		global cnt
+		cnt = cnt + 1
+		return JsonResponse({"name": cnt, "value": randrange(0, 100)})
 
 class IndexView(APIView):
-    def get(self, request, *args, **kwargs):
-        # return HttpResponse(content=open("./templates/index.html").read())
-        return render(request, 'chart/index.html')
+	def get(self, request, *args, **kwargs):
+		# return HttpResponse(content=open("./templates/index.html").read())
+		return render(request, 'chart/index.html')
 
 class CurrentView(APIView):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'chart/current.html')
+	def get(self, request, *args, **kwargs):
+		return render(request, 'chart/current.html')
 
 class VoltageView(APIView):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'chart/voltage.html')
+	def get(self, request, *args, **kwargs):
+		return render(request, 'chart/voltage.html')
 
 class TemperatureFirstView(APIView):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'chart/temperatureFirst.html')
+	def get(self, request, *args, **kwargs):
+		return render(request, 'chart/temperatureFirst.html')
 
 class TemperatureSecondView(APIView):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'chart/temperatureSecond.html')
+	def get(self, request, *args, **kwargs):
+		return render(request, 'chart/temperatureSecond.html')
 
 class TemperatureThirdView(APIView):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'chart/temperatureThird.html')
+	def get(self, request, *args, **kwargs):
+		return render(request, 'chart/temperatureThird.html')
